c45a1ad45dc45bc1cfc9c40a3fa60635d0a8873c
razaizim <zim.sarkar5956@gmail.com>
2025-01-23 08:35:56 +0600

    session created when login and redirect to verification page

--- login.php
+++ login.php
@@ -1,47 +1,41 @@
-
-<?php  
+<?php
 require_once('config.php');
 session_start();
 
-if(isset($_POST['login_form'])){
- 
+if (isset($_POST['login_form'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
- 
-    if(empty($username)){
-        $error = "Username is Required!";
-    }
-    elseif(empty($password)){
-        $error = "Password is Required!";
-    }
-    else{
-        $password = SHA1($password);
-       
-        $loginCount = $connection->prepare("SELECT id,email,mobile,username,password,email_status,mobile_status FROM users WHERE username=? AND password=?");
-        $loginCount->execute(array($username,$password));
-        $userloginCount = $loginCount->rowCount();
 
-        if($userloginCount == 1){
-            $userData =  $loginCount->fetch(PDO::FETCH_ASSOC);
+    if (empty($username)) {
+        $error = 'Username must not be empty';
+    } elseif (empty($password)) {
+        $error = 'Password must not be empty';
+    } else {
+        $password = sha1($password);
+        $user_info = $connection->prepare("SELECT id,email,mobile, mobile_status, email_status, username, password FROM users WHERE username = ? AND password =?");
+        $user_info->execute(array($username, $password));
+        $user_count = $user_info->rowCount();
 
-            if($userData['mobile_status'] == 1 AND $userData['email_status'] == 1){
-                $_SESSION['user']  = $userData;
+        if ($user_count == 1) {
+            $success = 'Login successfully';
+            $user_data = $user_info->fetch(PDO::FETCH_ASSOC);
+
+            if ($user_data['email_status'] == '1' && $user_data['mobile_status'] == 1) {
+                $_SESSION['user'] = $user_data;
                 header('location:index.php');
-            }
-            else{
+            } else {
                 header('location:verification.php');
             }
-        }
-        else{
-            $error = "Username or Password is Wrong!";
+        } else {
+            $error = 'Invalid username or password';
         }
     }
-
 }
 
-if(isset($_SESSION['user'])){
+if (isset($_SESSION['user'])) {
     header('location:index.php');
 }
+
 ?>
 <!DOCTYPE html>
 <html class="h-100" lang="en">
@@ -54,11 +48,11 @@ if(isset($_SESSION['user'])){
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
     <link href="css/style.css" rel="stylesheet">
-    
+
 </head>
 
 <body class="h-100">
-    
+
     <!--*******************
         Preloader start
     ********************-->
@@ -72,7 +66,7 @@ if(isset($_SESSION['user'])){
     <!--*******************
         Preloader end
     ********************-->
- 
+
     <div class="login-form-bg h-100">
         <div class="container h-100">
             <div class="row justify-content-center h-100">
@@ -80,18 +74,20 @@ if(isset($_SESSION['user'])){
                     <div class="form-input-content">
                         <div class="card login-form mb-0">
                             <div class="card-body pt-5">
-                                <a class="text-center" href="index.php"> <h2>Login</h2></a>
-                                <?php if(isset($error)):?>
-                                <div class="alert alert-danger">
-                                    <?php echo $error;  ?>
-                                </div>
+                                <a class="text-center" href="index.php">
+                                    <h2>Login</h2>
+                                </a>
+                                <?php if (isset($error)): ?>
+                                    <div class="alert alert-danger">
+                                        <?php echo $error;  ?>
+                                    </div>
                                 <?php endif; ?>
-                                <?php if(isset($success)):?>
-                                <div class="alert alert-success">
-                                    <?php echo $success;  ?>
-                                </div>
+                                <?php if (isset($success)): ?>
+                                    <div class="alert alert-success">
+                                        <?php echo $success;  ?>
+                                    </div>
                                 <?php endif; ?>
-                                <form  method="POST" action="" class="mt-5 mb-5 login-input">
+                                <form method="POST" action="" class="mt-5 mb-5 login-input">
                                     <div class="form-group">
                                         <input type="text" name="username" class="form-control" placeholder="Username">
                                     </div>
@@ -108,7 +104,7 @@ if(isset($_SESSION['user'])){
             </div>
         </div>
     </div>
-     
+
 
     <!--**********************************
         Scripts
@@ -119,9 +115,5 @@ if(isset($_SESSION['user'])){
     <script src="js/gleek.js"></script>
     <script src="js/styleSwitcher.js"></script>
 </body>
+
 </html>
-
-
-
-
-
